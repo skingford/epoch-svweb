ee6f2f8898cdc75a16c5ec8c832006fbb1be1c49
zjf <zhongfjinfu@moren.com>
2025-04-29 17:07:04 +0800

    chore(shadcn): shadcn init

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { fileURLToPath, URL } from 'node:url';
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   resolve: {
     alias: {
-      '$lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+      $lib: path.resolve("./src/lib"),
     }
   },
 })
