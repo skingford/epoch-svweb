a6164d0f9df8f0fcb117b012f2ea3792bf74833b
zjf <zhongfjinfu@moren.com>
2025-04-29 11:30:47 +0800

    chore(types): node types

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '$lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+    }
+  },
 })
